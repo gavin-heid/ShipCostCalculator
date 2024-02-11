4726d3d02cef7f4b026ac1fbc8eb1739ad9e6a42
Lab_04_Part1_Finished <gavin.heid04@gmail.com>
2024-02-11 01:49:47 -0500

    fixed code

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         final double salesTax = 0.02;
-        double itemPrice = 76; //simulated input
+        double itemPrice = 99; //simulated input
         double shippingCost = 0;
         double totalCost = 0;
         System.out.println("The cost of the inputed item is " + itemPrice);
-        if (1000000 <= itemPrice && itemPrice>= 100)//tests if itemprice is realistic value or greater then 100
+        if (1000000 >= itemPrice && itemPrice>= 100)//tests if itemprice is realistic value or greater then 100
         {
             System.out.println("The shipping for this item is free and the total cost is " + itemPrice);
         }
